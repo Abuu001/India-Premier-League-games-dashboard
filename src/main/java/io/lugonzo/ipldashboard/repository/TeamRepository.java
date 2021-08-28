85f0717463ae7c1be3af0c39aa1872ad3035c442
Abuu001 <abulugz@gmail.com>
2021-08-28 13:42:34 +0300

    team repository

--- src/main/java/io/lugonzo/ipldashboard/repository/TeamRepository.java
+++ src/main/java/io/lugonzo/ipldashboard/repository/TeamRepository.java
@@ -1,2 +1,12 @@
-package io.lugonzo.ipldashboard.repository;public class TeamRepository {
+package io.lugonzo.ipldashboard.repository;
+
+import io.lugonzo.ipldashboard.entity.Team;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository  extends CrudRepository<Team, Long> {
+
+    Team findByTeamName(String teamName);
+
 }
