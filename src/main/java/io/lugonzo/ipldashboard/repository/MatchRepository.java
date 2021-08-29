5c515896d478041a57efc4b81b3b0f222ffd9f08
Abuu001 <abulugz@gmail.com>
2021-08-30 00:12:53 +0300

    added query to fetch list of matches in range of years

--- src/main/java/io/lugonzo/ipldashboard/repository/MatchRepository.java
+++ src/main/java/io/lugonzo/ipldashboard/repository/MatchRepository.java
@@ -3,13 +3,26 @@ package io.lugonzo.ipldashboard.repository;
 import io.lugonzo.ipldashboard.entity.Match;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface MatchRepository extends CrudRepository<Match,Long> {
+
+   /* List<Match>  getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(
+            String teamName1, LocalDate date1 ,LocalDate date2,
+            String teamName2 ,LocalDate date3 ,LocalDate date4
+    );*/
+
+    @Query("SELECT m FROM Match m WHERE (m.team1 = :teamName OR m.team2 = :teamName) AND m.date BETWEEN :dateStart AND :dateEnd ORDER BY date DESC")
+    List<Match> getMatchesByTeamBetweenDates(@Param("teamName") String teamName,
+                                             @Param("dateStart") LocalDate dateStart,
+                                             @Param("dateEnd") LocalDate dateEnd);
 
     List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
 
