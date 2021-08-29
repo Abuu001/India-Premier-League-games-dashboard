e1abfb744f5efc08a7f052169beac03287c0dcd2
Abuu001 <abulugz@gmail.com>
2021-08-29 22:42:51 +0300

    Added crossorigin to be accessible by other routes

--- src/main/java/io/lugonzo/ipldashboard/controller/TeamController.java
+++ src/main/java/io/lugonzo/ipldashboard/controller/TeamController.java
@@ -6,6 +6,7 @@ import io.lugonzo.ipldashboard.repository.TeamRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 @NoArgsConstructor
+@CrossOrigin
 public class TeamController {
 
     @Autowired
