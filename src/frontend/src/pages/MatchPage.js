03f784a44fab7c9738f2b1892245b2f7da1c0cd5
Abuu001 <abulugz@gmail.com>
2021-08-30 00:11:21 +0300

    Matches for teams

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import {React , useEffect,useState} from 'react'
+import {useParams} from "react-router-dom";
+import axios from "axios"
+import MatchDetailCard from '../components/MatchDetailCard';
+
 
 function MatchPage() {
+
+    const[matches,setMatches]=useState([]);
+    const {teamName,year} =useParams(); 
+     
+    useEffect(
+        ()=>{
+     
+            fetchMatches();
+        },[] );
+
+    const fetchMatches = async ()=>{ 
+
+        await axios.get(`http://localhost:3002/team/${teamName}/matches?year=${year}`).then(response=>{
+     
+            setMatches([...response.data])
+            console.log(response.data); 
+          }) 
+        
+    }
+
     return (
-        <div>
-            
+        <div className="MatchPage">
+            <h1>Match Page</h1>
+
+            {matches.map(match =>{ 
+                return( 
+                        <MatchDetailCard  teamName={teamName}   match={match} /> 
+                )
+            })}
+
         </div>
     )
 }
