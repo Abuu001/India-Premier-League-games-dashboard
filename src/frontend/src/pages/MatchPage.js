3fb943eaae1c2d358a3138643ae4fbd97baea082
Abuu001 <abulugz@gmail.com>
2021-08-30 22:21:39 +0300

    Added style to set colors if won or lost the match

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -2,7 +2,8 @@ import {React , useEffect,useState} from 'react'
 import {useParams} from "react-router-dom";
 import axios from "axios"
 import MatchDetailCard from '../components/MatchDetailCard';
-
+import "./MatchPage.scss"
+import YearSelector from './YearSelector';
 
 function MatchPage() {
 
@@ -13,27 +14,35 @@ function MatchPage() {
         ()=>{
      
             fetchMatches();
-        },[] );
+        },[teamName,year] );
 
     const fetchMatches = async ()=>{ 
 
-        await axios.get(`http://localhost:3002/team/${teamName}/matches?year=${year}`).then(response=>{
+        await axios.get(`${process.env.REACT_APP_API_ROOT_URL}/team/${teamName}/matches?year=${year}`).then(response=>{
      
-            setMatches([...response.data])
-            console.log(response.data); 
+            setMatches([...response.data]) 
           }) 
-        
+         
     }
 
     return (
         <div className="MatchPage">
-            <h1>Match Page</h1>
+            <div className="year-selector">
+                    <h1>Select Year</h1>
+                    <YearSelector teamName={teamName} />
+            </div>
 
-            {matches.map(match =>{ 
-                return( 
-                        <MatchDetailCard  teamName={teamName}   match={match} /> 
-                )
-            })}
+            <div className="match-info">
+              <h1 className="page-heading">{teamName} matches in {year}</h1>
+                {matches.length===0  ?   <h4 id="no-match">No matches available</h4>
+                                 :  
+                matches.map(match =>{ 
+                    return( 
+                         <MatchDetailCard  key={match.id} teamName={teamName}   match={match} /> 
+                    )
+                })}
+            </div>
+       
 
         </div>
     )
