c79de3a9bd4311ae03b6a55cd649cf8c2b14651b
Abuu001 <abulugz@gmail.com>
2021-08-30 22:17:45 +0300

    added component tht displays all  match details

--- src/frontend/src/components/MatchDetailCard.js
+++ src/frontend/src/components/MatchDetailCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import "./MatchDetailCard.scss";
 
 function MatchDetailCard({teamName,match}) {
 
@@ -8,14 +9,33 @@ function MatchDetailCard({teamName,match}) {
     const otherTeam = match.team1 === teamName ?  match.team2 : match.team1;
     const otherTeamRoute = `/teams/${otherTeam}`;
 
-    return (
-        <div>
-            <h3>Latest matches</h3> 
-            <h1><i> vs   <Link to={otherTeamRoute}> {otherTeam} </Link></i></h1>
-            <h2>{match.date}</h2>
-            <h3>{match.venue}</h3>
+    const isMatchWon = teamName === match.matchWinner;
 
-            <h3>{match.matchWinner} won by {match.resultMargin}  {match.result}</h3>
+    return (
+        <div className= {isMatchWon ? 'MatchDetailCard  won-card' : 'MatchDetailCard  lost-card'}> 
+            <div>
+               <span className="vs">vs</span> 
+                <h1 ><i>   <Link to={otherTeamRoute}> {otherTeam} </Link></i></h1>
+                <h2 className="match-date">{match.date}</h2>
+                <h3 className="match-venue">{match.venue}</h3>
+
+                <h3 className="match-result">{match.matchWinner} won by {match.resultMargin}  {match.result}</h3>
+            </div>
+
+            <div className="additionalDetail">
+                <h3>First Innings</h3>
+                <p>{match.team1}</p>
+
+                <h3>Second Innings</h3>
+                <p>{match.team2}</p>
+
+                <h3>Man Of The Match</h3>
+                <p>{match.playerOfMatch}</p>
+
+                <h3>Umpires</h3>
+                <p>{match.umpire1} , {match.umpire2}</p>
+            </div>
+           
         </div>
     )
 }
